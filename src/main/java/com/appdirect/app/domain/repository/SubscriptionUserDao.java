ee90450e61933321b524073affb859f57f0ce42f
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-20 16:05:19 -0400

    Fixed typo

--- src/main/java/com/appdirect/app/domain/repository/SubscriptionUserDao.java
+++ src/main/java/com/appdirect/app/domain/repository/SubscriptionUserDao.java
@@ -2,7 +2,6 @@ package com.appdirect.app.domain.repository;
 
 
 
-import com.appdirect.app.domain.entity.Order;
 import com.appdirect.app.domain.entity.SubscriptionUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
