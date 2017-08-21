dbac79ed0064c36fb37fcd27debee3600c6c3a92
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-21 00:03:56 -0400

    Added SearchBy User UUID in UniqueId Validator

--- src/main/java/com/appdirect/app/domain/repository/SubscriptionUserDao.java
+++ src/main/java/com/appdirect/app/domain/repository/SubscriptionUserDao.java
@@ -2,10 +2,16 @@ package com.appdirect.app.domain.repository;
 
 
 
+import com.appdirect.app.domain.entity.Subscription;
 import com.appdirect.app.domain.entity.SubscriptionUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubscriptionUserDao extends JpaRepository<SubscriptionUser, Long> {
+
+    @Query("select s from SubscriptionUser s where s.uuid=?1")
+    Subscription findByUserUUID(String uuid);
+
 }
--- src/main/java/com/appdirect/app/validation/UniqueIdValidator.java
+++ src/main/java/com/appdirect/app/validation/UniqueIdValidator.java
@@ -1,9 +1,10 @@
 package com.appdirect.app.validation;
 
 
-
 import com.appdirect.app.domain.entity.Subscription;
+import com.appdirect.app.domain.entity.SubscriptionUser;
 import com.appdirect.app.domain.repository.SubscriptionDao;
+import com.appdirect.app.domain.repository.SubscriptionUserDao;
 import com.appdirect.app.dto.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,10 @@ public class UniqueIdValidator implements EventValidator {
     public void validateEvent(Event event, Class aClass, Serializable id, JpaRepository repository, RuntimeException exception) {
         if(Subscription.class.equals(aClass)) {
             if(((SubscriptionDao) repository).findByAccountIdentifier((String) id) != null) {
+                throw exception;
+            }
+        } if(SubscriptionUser.class.equals(aClass)) {
+            if(((SubscriptionUserDao) repository).findByUserUUID((String) id) != null) {
                 throw exception;
             }
         } else {
