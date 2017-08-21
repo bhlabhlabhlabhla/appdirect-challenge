5933ccdc482ece8af7fb4de35e3f69d5b1cf8542
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-20 22:24:24 -0400

    Added null checks on SubscriptionEntity Converter

--- src/main/java/com/appdirect/app/converter/SubscriptionEntityConverter.java
+++ src/main/java/com/appdirect/app/converter/SubscriptionEntityConverter.java
@@ -16,31 +16,43 @@ public class SubscriptionEntityConverter implements EntityConverter<Subscription
     @Override
     public Subscription toEntity(Event dto) {
         Subscription entity = new Subscription();
-        entity.setAccountIdentifier(dto.getPayload().getCompany().getUuid());
+        if(dto == null) return null;
 
-        // company
-        entity.setCompanyCountry(dto.getPayload().getCompany().getCountry());
-        entity.setCompanyName(dto.getPayload().getCompany().getName());
-        entity.setCompanyPhoneNumber(dto.getPayload().getCompany().getPhoneNumber());
-        entity.setCompanyUuid(dto.getPayload().getCompany().getUuid());
-        entity.setCompanyWebsite(dto.getPayload().getCompany().getWebsite());
+        if(dto.getMarketPlace()!=null) {
+            // marketPlate
+            entity.setBaseUrl(dto.getMarketPlace().getBaseUrl());
+            entity.setPartner(dto.getMarketPlace().getPartner());
+        }
 
-        // marketPlate
-        entity.setBaseUrl(dto.getMarketPlace().getBaseUrl());
-        entity.setPartner(dto.getMarketPlace().getPartner());
+        if(dto.getPayload()!= null) {
 
-        //order
-        entity.setEditionCode(dto.getPayload().getOrder().getEditionCode());
-        entity.setMaxOrderItems(dto.getPayload().getOrder().getItems()!=null?
-                dto.getPayload().getOrder().getItems().get(0).getQuantity():0);
-        entity.setPricingDuration(dto.getPayload().getOrder().getPricingDuration());
-        entity.setItems(dto.getPayload().getOrder().getItems().stream().map(item -> {
-            OrderItem orderItem = new OrderItem();
-            orderItem.setQuantity(item.getQuantity());
-            orderItem.setUnit(item.getUnit());
-            orderItem.setSubscription(entity);
-            return orderItem;
-        }).collect(Collectors.toSet()));
+            if(dto.getPayload().getCompany()!=null) {
+                entity.setAccountIdentifier(dto.getPayload().getCompany().getUuid());
+
+                // company
+                entity.setCompanyCountry(dto.getPayload().getCompany().getCountry());
+                entity.setCompanyName(dto.getPayload().getCompany().getName());
+                entity.setCompanyPhoneNumber(dto.getPayload().getCompany().getPhoneNumber());
+                entity.setCompanyUuid(dto.getPayload().getCompany().getUuid());
+                entity.setCompanyWebsite(dto.getPayload().getCompany().getWebsite());
+            }
+
+            if(dto.getPayload().getOrder()!=null) {
+                //order
+                entity.setEditionCode(dto.getPayload().getOrder().getEditionCode());
+                entity.setMaxOrderItems(dto.getPayload().getOrder().getItems()!=null?
+                        dto.getPayload().getOrder().getItems().get(0).getQuantity():0);
+                entity.setPricingDuration(dto.getPayload().getOrder().getPricingDuration());
+                entity.setItems(dto.getPayload().getOrder().getItems().stream().map(item -> {
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.setQuantity(item.getQuantity());
+                    orderItem.setUnit(item.getUnit());
+                    orderItem.setSubscription(entity);
+                    return orderItem;
+                }).collect(Collectors.toSet()));
+            }
+
+        }
 
         return entity;
     }
