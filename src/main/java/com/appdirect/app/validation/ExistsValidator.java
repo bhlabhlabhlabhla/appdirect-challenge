642bc01bbc037829bc64275c295ab12fb7b2cfe1
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-21 00:48:59 -0400

    Added support for SubscriptionExists Validator

--- src/main/java/com/appdirect/app/validation/ExistsValidator.java
+++ src/main/java/com/appdirect/app/validation/ExistsValidator.java
@@ -1,7 +1,9 @@
 package com.appdirect.app.validation;
 
 import com.appdirect.app.domain.entity.Subscription;
+import com.appdirect.app.domain.entity.SubscriptionUser;
 import com.appdirect.app.domain.repository.SubscriptionDao;
+import com.appdirect.app.domain.repository.SubscriptionUserDao;
 import com.appdirect.app.dto.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,10 @@ public class ExistsValidator implements EventValidator {
     public void validateEvent(Event event, Class tClazz, Serializable id, JpaRepository repository, RuntimeException e) {
         if(Subscription.class.equals(tClazz)) {
             if(((SubscriptionDao) repository).findByAccountIdentifier((String) id) == null) {
+                throw e;
+            }
+        } else if(SubscriptionUser.class.equals(tClazz)) {
+            if(((SubscriptionUserDao) repository).findByUserUUID((String) id) == null) {
                 throw e;
             }
         } else {
