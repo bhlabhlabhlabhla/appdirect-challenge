cf88a89dd7d060880b50c8e03130ce6f09186878
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-21 20:58:54 -0400

    Fixed UserLimitValidator to not account for current assignment

--- src/main/java/com/appdirect/app/validation/UserLimitValidator.java
+++ src/main/java/com/appdirect/app/validation/UserLimitValidator.java
@@ -3,7 +3,6 @@ package com.appdirect.app.validation;
 import com.appdirect.app.domain.entity.Subscription;
 import com.appdirect.app.domain.repository.SubscriptionDao;
 import com.appdirect.app.dto.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,7 @@ public class UserLimitValidator implements EventValidator {
     @Override
     public void validateEvent(Event event, Class tClazz, Serializable serializable, CrudRepository jpaRepository, RuntimeException e) {
         Subscription subscription = ((SubscriptionDao)jpaRepository).findByAccountIdentifier(event.getPayload().getAccount().getAccountIdentifier());
-        if(subscription.getItems()!=null && subscription.getItems().size() + 1 > subscription.getMaxOrderItems())
+        if(subscription.getItems()!=null && subscription.getItems().size() > subscription.getMaxOrderItems())
             throw e;
 
     }
