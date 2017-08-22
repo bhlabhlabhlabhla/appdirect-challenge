13087cf45fc2634505052bc862030b6dbc9e2c2c
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-21 20:48:58 -0400

    Removed Import Script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,15 @@
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (1,'1asdasd','1qweqweqwe');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (2,'2asdasd','2qweqweqwe');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (11,'11asdasd','asdf3');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (21,'21asdasd','246jed');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (12,'12asdasd','vaewru');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (22,'22asdasd','24nh65');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (13,'31asdasd','seht4');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (23,'42asdasd','n3654ju');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (14,'16asdasd','b245h');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (25,'28asdasd','ntj466');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (16,'21ajsdasd','aetj57i');
-insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (27,'212asdasd','qbh35u');
-insert into subscription_user (id,first_name,last_name,subscription_id,administrator) values (1,'321534','asdfasfsdf',1,true);
-
-update subscription set max_Order_Items=2;
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (1,'1asdasd','1qweqweqwe');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (2,'2asdasd','2qweqweqwe');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (11,'11asdasd','asdf3');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (21,'21asdasd','246jed');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (12,'12asdasd','vaewru');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (22,'22asdasd','24nh65');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (13,'31asdasd','seht4');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (23,'42asdasd','n3654ju');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (14,'16asdasd','b245h');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (25,'28asdasd','ntj466');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (16,'21ajsdasd','aetj57i');
+-- insert into subscription (id,ACCOUNT_IDENTIFIER,company_Uuid) values (27,'212asdasd','qbh35u');
+-- insert into subscription_user (id,first_name,last_name,subscription_id,administrator) values (1,'321534','asdfasfsdf',1,true);
+--
+-- update subscription set max_Order_Items=2;
