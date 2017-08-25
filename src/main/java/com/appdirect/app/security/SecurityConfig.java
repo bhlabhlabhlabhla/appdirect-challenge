4c424b3225e4ab73ab27fd55d38f604a38a1fd40
JagdeepSingh <jagdeep1490@gmail.com>
2017-08-24 20:59:00 -0400

    Update Security Configuration

--- src/main/java/com/appdirect/app/security/SecurityConfig.java
+++ src/main/java/com/appdirect/app/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth.common.signature.SharedConsumerSecretImpl;
 import org.springframework.security.oauth.consumer.BaseProtectedResourceDetails;
 import org.springframework.security.oauth.consumer.ProtectedResourceDetails;
@@ -18,14 +17,11 @@ import org.springframework.security.oauth.provider.*;
 import org.springframework.security.oauth.provider.filter.OAuthProviderProcessingFilter;
 import org.springframework.security.oauth.provider.filter.ProtectedResourceProcessingFilter;
 import org.springframework.security.oauth.provider.token.InMemoryProviderTokenServices;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.session.ConcurrentSessionFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -66,8 +62,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().anonymous();
 
         http.antMatcher("/api/v1/integration/**")
-            //.exceptionHandling().authenticationEntryPoint(new UnauthorizedRequestExceptionHandler())
-            //.and()
             .authorizeRequests().anyRequest().permitAll()
             .and()
             .addFilterBefore(oAuthProviderProcessingFilter(), ConcurrentSessionFilter.class);
@@ -75,7 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * Custom Filter to handle oAuth Signature verification.
-     * It instantiates Spring's OAuthProcessingFilterEntryPoint with consumer key and consumer secret from AppDirect security configuration
+     * It instantiates Spring OAuthProcessingFilterEntryPoint with consumer key and consumer secret from AppDirect security configuration
      * @return Custom Filter
      */
     @Bean
@@ -140,15 +134,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return resource;
     }
 
-    private class UnauthorizedRequestExceptionHandler implements AuthenticationEntryPoint {
-
-        private Logger logger = LoggerFactory.getLogger(UnauthorizedRequestExceptionHandler.class);
-
-        @Override
-        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-            logger.info("Request not authorized");
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-        }
-
-    }
 }
